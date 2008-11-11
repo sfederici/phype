b75d3c581584437f050b47413951493108e40a72
casperbp <casperbp@5ef258aa-aa63-11dd-9ddb-6f86aeb5eef6>
2008-11-11 12:59:46 +0000

    Parsing correct with strings longer than 16 chars.

--- src/interpret.js
+++ src/interpret.js
@@ -1,4 +1,3 @@
-
 function importPHPScripts() {
 	var phpScripts = [];
 	var scripts = document.getElementsByTagName('script');
@@ -34,41 +33,59 @@ window.onload = function(){
 function interpret(phypeCodes) {
 	for (index in phypeCodes) {
 		if (index != 'function_table' && index != 'class_table') {
-			var op = parsePhypeCode(phypeCodes[index]);
-			//alert('code: '+op.code+', arg1: '+op.arg1+', arg2: '+op.arg2+', arg3: '+op.arg3);
+			var op = opcodeParser.parse(phypeCodes[index]);
+			alert('code: '+op.code+', arg1: '+op.arg1+', arg2: '+op.arg2+', arg3: '+op.arg3);
 		}
 	}
 }
 
-/**
- * UTILS
- */
+/***********
+ * HELPERS *
+ ***********/
+var opcodeParser = {
+	/**
+	 * Takes a parsekit formatted opcode string and parses it into a JSON object with the properties:
+	 *  - command: The name of the opcode.
+	 *  - arg1: First argument.
+	 *  - arg2: Second argument.
+	 *  - arg3: Third argument.
+	 * 
+	 * @param {String} phypeCode The opcode string to parse.
+	 */
+	parse : function(phypeCode) {
+		var json = {};
+		
+		var firstSpace = phypeCode.indexOf(' ');
+		json.code = phypeCode.substring(0,firstSpace);
+		
+		var argStr = phypeCode.substring(firstSpace,phypeCode.length);
+		json.arg1 = argStr.match(/('[^']*'|UNUSED|NULL|T\([0-9]+\)|[0-9]+|0x[a-fA-F0-9]+)/)[0];
+		json.arg1 = opcodeParser.fixString(json.arg1);
+		
+		argStr = argStr.substring(json.arg1.length,argStr.length);
+		json.arg2 = argStr.match(/('[^']*'|UNUSED|NULL|T\([0-9]+\)|[0-9]+|0x[a-fA-F0-9]+)/)[0];
+		json.arg2 = opcodeParser.fixString(json.arg2);
+		
+		argStr = argStr.substring(json.arg2.length,argStr.length);
+		json.arg3 = argStr.match(/('[^']*'|UNUSED|NULL|T\([0-9]+\)|[0-9]+|0x[a-fA-F0-9]+)/)[0];
+		json.arg3 = opcodeParser.fixString(json.arg3);
+		
+		return json;
+	},
+	
+	fixString : function(str) {
+		if (str.indexOf('\'')==0 && str.length > 19)
+			str = str.substring(0, str.length-4)+'\'';
+		
+		return str;
+	}
+}
+
+/*********
+ * UTILS *
+ *********/
 var ajax={};
 ajax.x=function(){try{return new ActiveXObject('Msxml2.XMLHTTP')}catch(e){try{return new ActiveXObject('Microsoft.XMLHTTP')}catch(e){return new XMLHttpRequest()}}};
 ajax.send=function(u,f,m,a){var x=ajax.x();x.open(m,u,true);x.onreadystatechange=function(){if(x.readyState==4)f(x.responseText)};if(m=='POST')x.setRequestHeader('Content-type','application/x-www-form-urlencoded');x.send(a)};
 ajax.gets=function(url){var x=ajax.x();x.open('GET',url,false);x.send(null);return x.responseText};
 ajax.get=function(url,func){ajax.send(url,func,'GET')};
-
-/**
- * Takes a parsekit formatted opcode string and parses it into a JSON object with the properties:
- *  - command: The name of the opcode.
- *  - arg1: First argument.
- *  - arg2: Second argument.
- *  - arg3: Third argument.
- * 
- * @param {String} phypeCode The opcode string to parse.
- */
-function parsePhypeCode(phypeCode) {
-	var json = {};
-	
-	var firstSpace = phypeCode.indexOf(' ');
-	json.code = phypeCode.substring(0,firstSpace);
-	var argStr = phypeCode.substring(firstSpace,phypeCode.length);
-	json.arg1 = argStr.match(/('[^']*'|UNUSED|NULL|T\([0-9]+\)|[0-9]+|0x[a-fA-F0-9]+)/)[0];
-	argStr = argStr.substring(json.arg1.length,argStr.length);
-	json.arg2 = argStr.match(/('[^']*'|UNUSED|NULL|T\([0-9]+\)|[0-9]+|0x[a-fA-F0-9]+)/)[0];
-	argStr = argStr.substring(json.arg2.length,argStr.length);
-	json.arg3 = argStr.match(/('[^']*'|UNUSED|NULL|T\([0-9]+\)|[0-9]+|0x[a-fA-F0-9]+)/)[0];
-	
-	return json;
-}
