3f4ccf3f0e6d51fb1dbbe680107338b9dac2bf32
casperbp <casperbp@5ef258aa-aa63-11dd-9ddb-6f86aeb5eef6>
2008-11-11 18:41:57 +0000

    Refactoring to enable imports.

---
+++ src/phype.js
@@ -0,0 +1,15 @@
+// Imports
+function importScript(scriptName) {
+	var script = document.createElement('script');
+	script.src = scriptName;
+	script.type = 'text/javascript';
+	script.defer = true;
+	
+	// Insert the created object to the html head element
+	var head = document.getElementsByTagName('head').item(0);
+	head.appendChild(script);
+}
+
+importScript('src/utils.js');
+importScript('src/ops.js');
+importScript('src/interpret.js');
