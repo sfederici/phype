ff17a61f9fcb746ea0f04f91bd1b1012b98a9b2b
casperbp <casperbp@5ef258aa-aa63-11dd-9ddb-6f86aeb5eef6>
2008-11-11 12:37:40 +0000

    Opcode-parsing.

--- trunk/src/interpret.js
+++ trunk/src/interpret.js
@@ -27,13 +27,16 @@ window.onload = function(){
 }
 
 /**
- * Interprets an array of ZEND byte codes in JSON-format.
+ * Interprets an array of JSON-objects with parsekit formatted opcodes.
  * 
+ * @param {Array} phypeCodes An array of JSON-objects with parsekit formatted opcodes.
  */
 function interpret(phypeCodes) {
-	for (code in phypeCodes) {
-		if (code != '')
-		alert(phypeCodes[code]);
+	for (index in phypeCodes) {
+		if (index != 'function_table' && index != 'class_table') {
+			var op = parsePhypeCode(phypeCodes[index]);
+			//alert('code: '+op.code+', arg1: '+op.arg1+', arg2: '+op.arg2+', arg3: '+op.arg3);
+		}
 	}
 }
 
@@ -45,3 +48,27 @@ ajax.x=function(){try{return new ActiveXObject('Msxml2.XMLHTTP')}catch(e){try{re
 ajax.send=function(u,f,m,a){var x=ajax.x();x.open(m,u,true);x.onreadystatechange=function(){if(x.readyState==4)f(x.responseText)};if(m=='POST')x.setRequestHeader('Content-type','application/x-www-form-urlencoded');x.send(a)};
 ajax.gets=function(url){var x=ajax.x();x.open('GET',url,false);x.send(null);return x.responseText};
 ajax.get=function(url,func){ajax.send(url,func,'GET')};
+
+/**
+ * Takes a parsekit formatted opcode string and parses it into a JSON object with the properties:
+ *  - command: The name of the opcode.
+ *  - arg1: First argument.
+ *  - arg2: Second argument.
+ *  - arg3: Third argument.
+ * 
+ * @param {String} phypeCode The opcode string to parse.
+ */
+function parsePhypeCode(phypeCode) {
+	var json = {};
+	
+	var firstSpace = phypeCode.indexOf(' ');
+	json.code = phypeCode.substring(0,firstSpace);
+	var argStr = phypeCode.substring(firstSpace,phypeCode.length);
+	json.arg1 = argStr.match(/('[^']*'|UNUSED|NULL|T\([0-9]+\)|[0-9]+|0x[a-fA-F0-9]+)/)[0];
+	argStr = argStr.substring(json.arg1.length,argStr.length);
+	json.arg2 = argStr.match(/('[^']*'|UNUSED|NULL|T\([0-9]+\)|[0-9]+|0x[a-fA-F0-9]+)/)[0];
+	argStr = argStr.substring(json.arg2.length,argStr.length);
+	json.arg3 = argStr.match(/('[^']*'|UNUSED|NULL|T\([0-9]+\)|[0-9]+|0x[a-fA-F0-9]+)/)[0];
+	
+	return json;
+}
