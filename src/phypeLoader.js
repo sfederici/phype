926cab35dc498996114fb85f42bab5490b57442d
casperbp <casperbp@5ef258aa-aa63-11dd-9ddb-6f86aeb5eef6>
2008-11-27 09:23:48 +0000

    phypeWindow --> phypeDoc. Commented util functions.

--- src/phypeLoader.js
+++ src/phypeLoader.js
@@ -16,9 +16,11 @@ function loadPHP() {
 // Set our phypeIn-variable (the return of this function will be parsed by our phypeParser).
 var phypeIn = loadPHP;
 
+// Set our phypeDoc-variable. This should contain the document that phype should output to.
+var phypeDoc = document;
+
 // Set our phypeOut-variable (this function takes the generated parser-output, and should
 // output this somewhere appropriate).
-var phypeWindow = document;
 var phypeOut = function(out) {
-	phypeWindow.write(out);
+	phypeDoc.write(out);
 }
