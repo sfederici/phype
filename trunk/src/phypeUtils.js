f0f83f2febdcdf35ed8373311e1b67369b556413
casperbp <casperbp@5ef258aa-aa63-11dd-9ddb-6f86aeb5eef6>
2008-11-27 09:23:48 +0000

    phypeWindow --> phypeDoc. Commented util functions.

--- trunk/src/phypeUtils.js
+++ trunk/src/phypeUtils.js
@@ -1,6 +1,6 @@
-//////////
-// AJAX //
-//////////
+/**
+ * Borrowed from http://snippets.dzone.com/posts/show/2025
+ */
 var ajax={};
 ajax.x=function(){try{return new ActiveXObject('Msxml2.XMLHTTP')}catch(e){try{return new ActiveXObject('Microsoft.XMLHTTP')}catch(e){return new XMLHttpRequest()}}};
 ajax.send=function(u,f,m,a){var x=ajax.x();x.open(m,u,true);x.onreadystatechange=function(){if(x.readyState==4)f(x.responseText)};if(m=='POST')x.setRequestHeader('Content-type','application/x-www-form-urlencoded');x.send(a)};
